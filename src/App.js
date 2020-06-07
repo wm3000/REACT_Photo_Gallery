0df2ded3d738418faf3bdefa7eb9a63c5d6d69b6
Waldemar Mikluszka <wmikluszka@outlook.com>
2020-06-07 19:55:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <dialog open>
+          <h1>Pascuale</h1>
+          <p>Bimbo</p>
+          <footer><a href="https://typeofweb.com/">Tak!</a></footer>
+        </dialog>
+      </header>
+    </div>
+  );
+}
+
+export default App;
