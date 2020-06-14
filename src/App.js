9cf1c17859e56e86a163f7dd2e304ec215296d99
Waldemar Mikluszka <wmikluszka@outlook.com>
2020-06-14 17:40:39 +0200

    Photo Gallery added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,87 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+const photos = [
+  {
+    src: 'https://www.fujifilmusa.com/products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_008.JPG',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://www.cameraegg.org/wp-content/uploads/2013/02/Leica-M-Sample-Image-1.jpg',
+    width: 1365,
+    height: 911
+  },
+  {
+    src: 'https://www.fujifilmusa.com/products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_010.JPG',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://www.fujifilmusa.com/products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_011.JPG',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://www.cameraegg.org/wp-content/uploads/2013/02/Leica-M-Sample-Image.jpg',
+    width: 1365,
+    height: 911
+  },
+  {
+    src: 'https://www.fujifilmusa.com/products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_009.JPG',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://www.fujifilmusa.com/products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_010.JPG',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://www.fujifilmusa.com/products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_011.JPG',
+    width: 4,
+    height: 3
+  }
+
+];
+
 
 function App() {
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <dialog open>
-          <h1>Pascuale</h1>
-          <p>Bimbo</p>
-          <footer><a href="https://typeofweb.com/">Tak!</a></footer>
-        </dialog>
-      </header>
+      <header className="App-header">Header</header>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 }
