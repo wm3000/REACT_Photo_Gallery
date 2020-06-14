3e2610dbbf9f19e355600a942f0c25620ec5011e
Waldemar Mikluszka <wmikluszka@outlook.com>
2020-06-14 18:16:18 +0200

    Updates

---
+++ precache-manifest.2a4717f547577d85a9976bd0f28e3d1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744a16ec554dd2f75e8664a3d9a1a45b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "890d0bbdaf4a6ee8219c",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbe1126e1ce72826d8cf",
+    "url": "./static/js/2.d54cc3f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d54cc3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890d0bbdaf4a6ee8219c",
+    "url": "./static/js/main.8d9b7c0c.chunk.js"
+  },
+  {
+    "revision": "6af27e7d536bfdf28d27",
+    "url": "./static/js/runtime-main.765403d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
